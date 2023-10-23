613689ca8ffed0b4d52294d54f9d2e3098578de2
jsmorac <jhoansebastian78@hotmail.com>
2023-10-23 16:15:03 -0500

    Diseño prototipo del home, se puede cambiar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,113 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="home">
+    <div class="background-image" :style="{ backgroundImage: 'url(' + fondox + ')' }"></div>
+    <div class="icon-container">
+      <a @click="goToCreatePatient" class="icon">
+        <img src="../assets/icono11.png" alt="Create Patient">
+        <h2>Create Patient</h2>
+        <p class="descripcion">Crear un nuevo paciente</p>
+      </a>
+      <a @click="goToListUsers" class="icon">
+        <img src="../assets/icono22.png" alt="List Users">
+        <h2>List Users</h2>
+        <p class="descripcion">Ver la lista de usuarios</p>
+      </a>
+      <a @click="goToSearchUser" class="icon">
+        <img src="../assets/icon3.png" alt="Search User">
+        <h2>Search User</h2>
+        <p class="descripcion">Buscar un usuario específico</p>
+      </a>
+    <div class="copyright-container">
+      <div class="copyright">
+        &copy; 2023 MaizenaScript. Todos los derechos reservados.
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      fondox: require('../assets/fon2.avif'),
+    };
+  },
+  methods: {
+    goToCreatePatient() {
+      this.$router.push('/CUser');
+    },
+    goToListUsers() {
+      this.$router.push('/lUser');
+    },
+    goToSearchUser() {
+      this.$router.push('/sUser');
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.home {
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.background-image {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background-size: cover;
+  background-position: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.icon-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5vh; /* Puedes ajustar este valor para cambiar la posición vertical */
 }
-a {
-  color: #42b983;
+
+.icon {
+  margin: 5vw 5vw; /* Otra opción puede ser usar porcentajes como 5% */
+  cursor: pointer;
+  text-decoration: none;
+  color: rgb(75, 24, 244);
 }
+
+.icon h2 {
+  margin: 5px 0;
+}
+
+.icon p {
+  margin: 5px 0;
+}
+
+.icon:hover {
+  opacity: 0.9;
+  transform: scale(1.2); /* Esto hace que la imagen crezca al pasar el mouse sobre ella */
+  color: rgb(0, 88, 29);
+  font-weight: bold;
+}
+
+.icon img {
+  width: 13vw; /* Usa vw o porcentajes en lugar de píxeles */
+  height: auto;
+}
+.copyright-container {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 115px;
+    background-color: #70b3e0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .copyright {
+    color: white;
+  }
 </style>
