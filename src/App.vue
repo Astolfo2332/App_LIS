1dfe863b57882ab11180691d9c43de9996dbce22
unknown <luis.ramirez12@udea.edu.co>
2023-10-26 16:45:00 -0500

    cambie de posicion el menu con el logo y actualice el desplegable

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/coco.png" alt="Logo" class="logo">
-    <button @click="toggleMenu" class="menu-toggle">☰</button>
-    <nav :class="{ 'nav-links': true, 'show': showMenu }">
-      <ul :class="{ 'centered': showMenu }">
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/CUser">Crear paciente</router-link></li>
-        <li><router-link to="/lUser">Lista usuarios</router-link></li>
-        <li><router-link to="/sUser">Buscar usuario</router-link></li>
+    <div class="sidebar" :class="{ 'show': showMenu }">
+      <ul>
+        <li><router-link to="/" class="menu-link">Home</router-link></li>
+        <li><router-link to="/CUser" class="menu-link">Crear paciente</router-link></li>
+        <li><router-link to="/lUser" class="menu-link">Lista usuarios</router-link></li>
+        <li><router-link to="/sUser" class="menu-link">Buscar usuario</router-link></li>
       </ul>
-    </nav>
-    <router-view/>
+    </div>
+    <button @click="toggleMenu" class="menu-toggle" :class="{ 'show': showMenu }">☰</button>
+    <img src="./assets/coco.png" alt="Logo" class="logo" :class="{ 'show': showMenu }">
+    <router-view />
   </div>
 </template>
 
@@ -20,60 +20,71 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #503d2c;
   position: relative;
 }
 
 .logo {
-  position: absolute; 
-  top: 0%; 
-  left: 2%; 
-  width: 5%; 
-  height: auto; 
-}
-
-nav {
-  padding: 2%;
-  background: linear-gradient(to right, #6b6b6b, #ffffff);
-  text-align: right; 
-}
-
-.nav-links {
-  display: none; 
-}
-.nav-links.show {
-  display: block; 
-}
-
-.nav-links ul {
-  list-style: none;
-  padding: 0;
-}
-
-.nav-links ul li {
-  display: inline; 
-  margin: 0 1%; 
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  position: absolute;
+  top: 0%;
+  right: 2%;
+  width: 5%;
+  height: auto;
+  
 }
 
 .menu-toggle {
   position: absolute;
-  top: 20px;
-  right: 20px;
+  top: 10px;
+  left: 2%; 
   font-size: 1.5em;
   cursor: pointer;
+  z-index: 2;
 }
 .centered {
-  text-align: center; /* Centra los elementos de la lista horizontalmente */
+  text-align: center;
 }
+
+.sidebar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 14%; 
+  height: 100%;
+  background: linear-gradient(to right, #70b3e0, #ffffff);
+  transition: transform 0.5s ease-in-out;
+  transform: translateX(-100%);
+}
+.sidebar ul li a {
+  font-size: 1.2em; 
+  color: #121212; 
+  text-decoration: none; 
+}
+.sidebar ul li a:hover{
+  color:rgb(0, 122, 36) ;
+}
+.sidebar ul li a.router-link-exact-active{
+  color: rgb(0, 122, 36) ;
+  font-weight: bold;
+}
+
+.sidebar.show {
+  transform: translateX(0);
+}
+
+.sidebar ul {
+  list-style: none;
+  padding: 10% 0%;
+}
+
+.sidebar ul li {
+  margin: 5% 0;
+}
+
+.menu-toggle.show {
+  left: 13.4%; 
+}
+
 </style>
 
 <script>
